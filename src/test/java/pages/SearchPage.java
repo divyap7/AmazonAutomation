afcbad41e5a9d30ada218620d660f3d074775b79
Divya Premkumar <Divya Premkumar@Eli007>
2019-03-24 15:37:58 +0530

    #updated search page

--- src/test/java/pages/SearchPage.java
+++ src/test/java/pages/SearchPage.java
@@ -31,17 +31,34 @@ import cucumber.api.DataTable;
 public class SearchPage extends ActionLib {
 	final static Logger logger = LoggerFactory.getLogger(SearchPage.class);
 	ExtentReporterClass objExtentReports = new ExtentReporterClass();
+	By enterSearchWait = By.cssSelector("input#twotabsearchtextbox");
+	By waitForSuggestions = By.cssSelector("div#suggestions");
+	By clickAndGoWait = By.cssSelector("input[value=Go]")
+	String getItemName = "div#issDiv%d";
+	String bookName = "//div[@data-index='%d']//div[@class='sg-row']//h5/a[contains(@class,'text-normal')]/span";
+	String author = "(//div[@data-index='%d']//div[@class='sg-row']//span[contains(text(),'by')]//following-sibling::a)[%d]";
+	String getType = "(//div[@data-index='%d']//div[@class='sg-row']//a[contains(@class,'text-bold')])[%d]";
+	String getPrice = "(//div[@data-index='%d']//div[@class='sg-row']//span[@class='a-price' and @data-a-size='l']//span[@class='a-price-whole'])[%d]";
+	String getFraction = "(//div[@data-index='%d']//div[@class='sg-row']//span[@class='a-price' and @data-a-size='l']//span[@class='a-price-fraction'])[%d]";
+	String getNoOfAuthors = "//div[@data-index='%d']//div[@class='sg-row']//span[contains(text(),'by')]//following-sibling::a";
+	String getNoOfTypes = "//div[@data-index='%d']//div[@class='sg-row']//a[contains(@class,'text-bold')]";
 
 	public SearchPage(WebDriver driver) {
 		this.driver = driver;
 		PageFactory.initElements(driver, this);
 	}
 
-	@FindBy(xpath = "//input[@id='twotabsearchtextbox']")
+	@FindBy(css = "input#twotabsearchtextbox")
 	WebElement enterSearch;
 
-	@FindBy(xpath = "//input[@value='Go']")
+	@FindBy(css = "input[value=Go]")
 	WebElement clickAndGo;
+
+	@FindBy(css = "div#suggestions>div")
+	List<WebElement> suggestionList;
+
+	@FindBy(xpath = "(//div[@id='search']//div//span)[3]")
+	WebElement getItemDisplayed;
 
 	public void navigateToHomePage() {
 		try {
@@ -88,7 +105,7 @@ public class SearchPage extends ActionLib {
 			// To check if the string is not empty
 			Assert.assertNotNull("Fetched category", item);
 			// wait until the element is present
-			waitForElementToBePresent(By.xpath("//input[@id='twotabsearchtextbox']"));
+			waitForElementToBePresent(enterSearch);
 
 			enterSearch.clear();
 			enterSearch.click();
@@ -111,22 +128,21 @@ public class SearchPage extends ActionLib {
 			// Calling the method to enter item name in search textbox
 			this.enterSearch(input);
 
-			waitForElementToBePresent(By.xpath("//div[@id='suggestions']//div"));
+			waitForElementToBePresent(waitForSuggestions);
 			// fetch the count for list of suggestions displayed
-			int noOfSuggestions = driver.findElements(By.xpath("//div[@id='suggestions']//div")).size();
+			int noOfSuggestions = suggestionsList.size();
 
 			String searchedItem = sheetMap.get("Search").get(data.get(0).get("TestScenarioName")).get("item");
 			// To check if the string is not empty
 			Assert.assertNotNull("Fetched category", searchedItem);
-			// Iterates through the list and clicks the one that matched with the input
+			// Iterates through the list and clicks the one that matched with
+			// the input
 			for (int i = 0; i <= noOfSuggestions; i++) {
-				String itemInSuggestions = driver
-						.findElement(By.xpath("//div[@id='suggestions']//div[@id='issDiv" + i + "']"))
-						.getAttribute("data-keyword");
+				String itemInSuggestions = getItemName(i).getAttribute("data-keyword");
 				// To check if the string is not empty
 				Assert.assertNotNull("Fetched category", itemInSuggestions);
 				if (itemInSuggestions.equalsIgnoreCase(searchedItem)) {
-					driver.findElement(By.xpath("//div[@id='suggestions']//div[@id='issDiv" + i + "']")).click();
+					getItemName(i).click();
 					logger.info("Selected: " + itemInSuggestions);
 					break;
 				} else {
@@ -146,15 +162,15 @@ public class SearchPage extends ActionLib {
 			String itemSearched = sheetMap.get("Search").get(data.get(0).get("TestScenarioName")).get("item");
 			// To check if the string is not empty
 			Assert.assertNotNull("Fetched category", itemSearched);
-			waitForElementToBePresent(By.xpath("//input[@value='Go']"));
+			waitForElementToBePresent(clickAndGoWait);
 
 			clickAndGo.click();
 			logger.info("Clicked on search");
-			//fetch the name of the item that is displayed
-			String itemDisplayed = driver.findElement(By.xpath("(//div[@id='search']//div//span)[3]")).getText()
-					.replaceAll("\"", "");
+			// fetch the name of the item that is displayed
+			String itemDisplayed = getItemDisplayed.getText().replaceAll("\"", "");
 			// To check if the results are based on search
-			Assert.assertTrue("Results are based on search", StringUtils.containsIgnoreCase(itemSearched, itemDisplayed));
+			Assert.assertTrue("Results are based on search",
+					StringUtils.containsIgnoreCase(itemSearched, itemDisplayed));
 
 		} catch (Exception ex) {
 			logger.error("Exception occured while navigating to clickOnSearch  due to :  " + ex.getMessage());
@@ -167,49 +183,40 @@ public class SearchPage extends ActionLib {
 
 			List<Map<String, String>> data = input.asMaps(String.class, String.class);
 			ArrayList<String> authorNames = new ArrayList<String>();
-			
-			// Gets the number of the result for whic the informations has to be extracted
+
+			// Gets the number of the result for whic the informations has to be
+			// extracted
 			String bookToPick = sheetMap.get("Search").get(data.get(0).get("TestScenarioName")).get("booktopick");
 
 			// fetch name of the book
 
-			String nameOfTheBook = driver.findElement(By.xpath("//div[@data-index='" + bookToPick
-					+ "']//div[@class='sg-row']//h5/a[contains(@class,'text-normal')]/span")).getText();
+			String nameOfTheBook = getBookName(bookToPick).getText();
 			this.writeDataToExcel(1, 0, nameOfTheBook);
-			
+
 			// fetch its authors
 
-			int noOfAuthors = driver.findElements(By.xpath("//div[@data-index='" + bookToPick
-					+ "']//div[@class='sg-row']//span[contains(text(),'by')]//following-sibling::a")).size();
+			int noOfAuthors = fetchList(getNoOfAuthor,bookToPick).size();
 
 			for (int i = 1; i <= noOfAuthors; i++) {
-				String author = driver.findElement(By.xpath("(//div[@data-index='" + bookToPick
-						+ "']//div[@class='sg-row']//span[contains(text(),'by')]//following-sibling::a)[" + i + "]"))
-						.getText();
-				authorNames.add(author);
+				String authorName = fetchInfo(author, bookToPick, i).getText();
+				authorNames.add(authorName);
 			}
 			// Store the data in excel
 			this.writeDataToExcel(1, 1, authorNames.toString());
 
 			// fetch its type and price
-			int types = driver.findElements(By.xpath(
-					"//div[@data-index='" + bookToPick + "']//div[@class='sg-row']//a[contains(@class,'text-bold')]"))
+			int types = fetchList(getNoOfTypes,bookToPick);
 					.size();
 			// in case of multiple types , iterate and fetch all the details
 			for (int i = 1; i <= types; i++) {
-				String type = driver.findElement(By.xpath("(//div[@data-index='" + bookToPick
-						+ "']//div[@class='sg-row']//a[contains(@class,'text-bold')])[" + i + "]")).getText();
-				String price = driver.findElement(By.xpath("(//div[@data-index='" + bookToPick
-						+ "']//div[@class='sg-row']//span[@class='a-price' and @data-a-size='l']//span[@class='a-price-whole'])["
-						+ i + "]")).getText();
-				String fraction = driver.findElement(By.xpath("(//div[@data-index='" + bookToPick
-						+ "']//div[@class='sg-row']//span[@class='a-price' and @data-a-size='l']//span[@class='a-price-fraction'])["
-						+ i + "]")).getText();
+				String type = fetchInfo(getType, bookToPick, i).getText();
+				String price = fetchInfo(getPrice,bookToPick,i).getText();
+				String fraction = fetchInfo(getFraction,bookToPick,i).getText();
 				String wholePrice = price + "." + fraction;
 				this.writeDataToExcel(i, 2, type);
 				this.writeDataToExcel(i, 3, wholePrice);
 			}
-			//capture screenshot
+			// capture screenshot
 			objExtentReports.reportStep("After search", "PASS");
 
 		} catch (Exception ex) {
@@ -232,6 +239,26 @@ public class SearchPage extends ActionLib {
 			logger.error("Exception occured while navigating to writeDataFromExcel  due to :  " + ex.getMessage());
 			Assert.fail("Exception occured while navigating to writeDataFromExcel due to :  " + ex.getMessage());
 		}
+	}
+
+	private WebElement getItemName(int i) {
+		WebElement itemName = driver.findElement(By.cssSelector(String.format(getItemName, i)));
+		return itemName;
+	}
+
+	private WebElement getBookName(String bookToPick) {
+		WebElement fetchBookName = driver.findElement(By.xpath(String.format(bookName, bookToPick)));
+		return fetchBookName;
+	}
+
+	private WebElement fetchInfo(String locator, String bookToPick, int i) {
+		WebElement element = driver.findElement(By.xpath(String.format(locator, bookToPick, i)));
+		return element;
+	}
+	
+	private List<WebElement> fetchList(String locator, String bookToPick) {
+		List<WebElement> element = driver.findElements(By.xpath(String.format(locator, bookToPick)));
+		return element;
 	}
 
 }
